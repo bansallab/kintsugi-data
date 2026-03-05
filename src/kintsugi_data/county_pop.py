1a0cbb88ba581c342784b9f7c82b78a9b5481d36
winter-again <63322884+winter-again@users.noreply.github.com>
2026-03-05 13:22:14 -0500

    Modify county population data to include all years. Decisions on further filtering can live in the actual package.

--- src/kintsugi_data/county_pop.py
+++ src/kintsugi_data/county_pop.py
@@ -1,11 +1,29 @@
+from typing import NamedTuple
+
 import polars as pl
 
 from .globals import DATA, DATA_UNTRACKED
 
 
+class FileInfo(NamedTuple):
+    lb: int
+    year_lb: int
+    ub: int
+    year_ub: int
+
+
 def main() -> int:
-    year_lb = 2010
-    year_ub = 2024
+    year_bounds = {
+        2016: FileInfo(3, 2010, 9, 2016),
+        2017: FileInfo(3, 2010, 10, 2017),
+        2018: FileInfo(3, 2010, 11, 2018),
+        2019: FileInfo(3, 2010, 12, 2019),
+        2020: FileInfo(3, 2010, 13, 2020),
+        2021: FileInfo(2, 2020, 3, 2021),
+        2022: FileInfo(2, 2020, 4, 2022),
+        2023: FileInfo(2, 2020, 5, 2023),
+        2024: FileInfo(2, 2020, 6, 2024),
+    }
     age_grps = {
         0: "tot",
         1: "0-4",
@@ -29,18 +47,41 @@ def main() -> int:
     }
     age_grp_enum = pl.Enum(age_grps.values())
 
-    for year in range(2010, year_ub + 1):
-        if year_lb <= year <= 2019:
-            file = "pop/county_cc/CC-EST2020-ALLDATA.csv"
-        else:
-            file = "pop/county_cc/cc-est2024-alldata.csv"
-
+    for file in sorted((DATA_UNTRACKED / "pop/county_cc").glob("*.csv")):
+        year = int(file.name.split("-")[1][3:])
+        year_info = year_bounds[year]
         lf = (
-            pl.scan_csv(
-                DATA_UNTRACKED / file,
-                encoding="utf8-lossy",
-                null_values=["X"],
-                new_columns=[
+            (
+                pl.scan_csv(
+                    file,
+                    encoding="utf8-lossy",
+                    null_values=["X"],
+                    new_columns=[
+                        "summary_lvl",
+                        "state_fips",
+                        "county_fips",
+                        "state_name",
+                        "county_name",
+                        "year",
+                        "age_grp",
+                        "tot_pop",
+                        "tot_male",
+                        "tot_female",
+                    ],
+                    schema_overrides={
+                        "summary_lvl": pl.String,
+                        "state_fips": pl.String,
+                        "county_fips": pl.String,
+                        "state_name": pl.String,
+                        "county_name": pl.String,
+                        "year": pl.Int64,
+                        "age_grp": pl.Int64,
+                        "tot_pop": pl.Int64,
+                        "tot_male": pl.Int64,
+                        "tot_female": pl.Int64,
+                    },
+                )
+                .select(
                     "summary_lvl",
                     "state_fips",
                     "county_fips",
@@ -51,54 +92,31 @@ def main() -> int:
                     "tot_pop",
                     "tot_male",
                     "tot_female",
-                ],
-                schema_overrides={
-                    "summary_lvl": pl.String,
-                    "state_fips": pl.String,
-                    "county_fips": pl.String,
-                    "state_name": pl.String,
-                    "county_name": pl.String,
-                    "year": pl.Int64,
-                    "age_grp": pl.Int64,
-                    "tot_pop": pl.Int64,
-                    "tot_male": pl.Int64,
-                    "tot_female": pl.Int64,
-                },
-            )
-            .select(
-                "summary_lvl",
-                "state_fips",
-                "county_fips",
-                "state_name",
-                "county_name",
-                "year",
-                "age_grp",
-                "tot_pop",
-                "tot_male",
-                "tot_female",
-            )
-            .filter(
-                pl.col("summary_lvl") == "050",
-                pl.col("state_fips").is_between(pl.lit("01"), pl.lit("56")),
-                pl.col("year").is_between(3, 12)
-                if year_lb <= year <= 2019
-                else pl.col("year").is_between(2, 6),
+                )
+                .filter(
+                    pl.col("summary_lvl") == "050",
+                    pl.col("state_fips").is_between(pl.lit("01"), pl.lit("56")),
+                    pl.col("year").is_between(year_info.lb, year_info.ub),
+                )
             )
             .with_columns(
                 county_fips=pl.col("state_fips") + pl.col("county_fips"),
                 year=pl.col("year").replace_strict(
-                    {k: v for k, v in zip(range(3, 13), range(2010, 2020))}
-                    if year_lb <= year <= 2019
-                    else {k: v for k, v in zip(range(2, 7), range(2020, 2025))},
+                    {
+                        k: v
+                        for k, v in zip(
+                            range(year_info.lb, year_info.ub + 1),
+                            range(year_info.year_lb, year_info.year_ub + 1),
+                        )
+                    },
                     return_dtype=pl.Int64,
                 ),
                 age_grp=pl.col("age_grp")
                 .replace_strict(age_grps, return_dtype=pl.String)
                 .cast(age_grp_enum),
             )
-            .filter(pl.col("year") == year)
             .drop("summary_lvl", "state_fips")
-            .sort("county_fips", "age_grp")
+            .sort("county_fips", "year", "age_grp")
         )
 
         lf.sink_parquet(DATA / f"pop/county_cc/county_pop_{year}.parquet")
